b16b66a85f83d0c4044c2dec71d6072df9bca5e4
Syndikit <maxaustin92@gmail.com>
2020-09-28 20:43:32 -0400

    Modded the test lex() in main. Should be working with unicode now. No extra methods.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,6 @@
 package com.company;
 
-import java.util.Arrays;
-
 public class Main {
-
-    static char[] open_paren = new char[]{'('};
-    static char[] close_paren = new char[]{')'};
-    static char[] end_bool = {','};
-    static char[] begin_test = new char[]{'t', 'e', 's', 't'};
-    static char[] end_test = new char[]{'?'};
-    static char[] bool_type = new char[]{'b', 'o', 'o', 'l'};
-    static char[] true_1 = new char[]{'1'};
-    static char[] false_0 = new char[]{'0'};
-    static char[] negation = new char[]{'\''};
-    static char[] conjunction = new char[]{'^'};
-    static char[] disjunction = new char[]{'v'};
-    static char[] equivalence = new char[]{'<', '-', '>'};
-    static char[] implication = new char[]{'-', '>'};
 
     public void initialize(char[] sentence) {
 
@@ -24,73 +8,94 @@ public class Main {
     }
 
     public static boolean isAlpha(char target) {
-        char[] alpha = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+        int t = target;
         boolean in = false;
-        for (char t : alpha) {
-            if (t == target) {
-                in = true;
-                break;
-            }
+
+        if ((t > 64) && (t < 91) && !(t==86)) {
+            in = true;
+        } else if ((t > 96) && (t < 123)&& !(t==118)) {
+            in = true;
         }
         return in;
+    }
+
+    public static boolean isBool(int a, int b, int c, int d) {
+        boolean res = false;
+        if ((a == 98 || a == 66) && (b == 111 || b == 79) && (c == 111 || c == 79) && (d == 108 || d == 76)) {
+            res = true;
+        }
+
+        return res;
+    }
+
+    public static boolean isTest(int a, int b, int c, int d) {
+        boolean res = false;
+        if ((a == 84 || a == 116) && (b == 69 || b == 101) && (c == 83 || c == 115) && (d == 84 || d == 116)) {
+            res = true;
+        }
+
+        return res;
     }
 
     public static void main(String[] args) {
         //String sentence = "^vTEST bool ' P )";
         String sentence = "bool P = 1, bool Q = 0, test P -> Q <-> P' v Q?";
         String sentence2 = "bool P = 0, bool Q = 0, bool R = P' ^ Q', test R?";
-        //char[][] test = {{'<','-','>'},{'v'}};
+
         char[] test = sentence.toCharArray();
         char[] test2 = sentence2.toCharArray();
-        char[][] split_sent = new char[20][20];
-        char[] arr = new char[20];
-        int tok_num = 0;
 
-        //char[][] symbols = {open_paren,close_paren,end_bool,end_test,true_1,false_0,negation,conjunction,disjunction, {'>'},{'<'},{'-'}};
+        //symbols = {'(', ')', ',', '?', '1', '0', '\'', '^', 'v', '='};
+        int[] int_symbols = {40, 41, 44, 63, 49, 48, 39, 94, 118, 61};
+        String[] tokens = {"OPEN_PAREN", "CLOSE_PAREN", "END_BOOL", "END_TEST", "TRUE_LITERAL", "FALSE_LITERAL", "NEGATION", "CONJUNCTION", "DISJUNCTION", "ASSIGNMENT"};
 
-        char[] symbols = {'(', ')', ',', '?', '1', '0', '\'', '^', 'v', '='};
-        String[] tokens = {"OPEN_PAREN","CLOSE_PAREN","END_BOOL","END_TEST","TRUE_LITERAL","FALSE_LITERAL","NEGATION","CONJUNCTION","DISJUNCTION","ASSIGNMENT"};
 
         for (int i = 0; i < test.length; i++) {
 
-            for (int j = 0; j < symbols.length; j++) {
-                if (symbols[j]==test[i]) {
+            for (int j = 0; j < int_symbols.length; j++) {
+                if (int_symbols[j] == test[i]) {
                     System.out.println(tokens[j]);
+
                 }
             }
 
-            if (test[i] == 'b' && test[i + 1] == 'o' && test[i + 2] == 'o' && test[i + 3] == 'l') {
-                System.out.println("BEGIN_BOOL");
-                i+=3;
+
+            if (((i + 3) < test.length) && (isBool(test[i], test[i + 1], test[i + 2], test[i + 3]))) {
+                System.out.println(String.format("%c%c%c%c = BEGIN_BOOL", test[i], test[i + 1], test[i + 2], test[i + 3]));
+                i += 3;
+
+            } else if (((i + 3) < test.length) && isTest((int) test[i], (int) test[i + 1], (int) test[i + 2], (int) test[i + 3])) {
+                System.out.println(String.format("%c%c%c%c = BEGIN_TEST", test[i], test[i + 1], test[i + 2], test[i + 3]));
+                i += 3;
+
+            } else if (((i + 2) < test.length) && (test[i] == 60) && (test[i + 1] == 45) && (test[i + 2] == 62)) {
+                System.out.println(String.format("%c%c%c = EQUIVALENCE", test[i], test[i + 1], test[i+2]));
+                i += 2;
+
+            } else if (((i + 1) < test.length) && (test[i] == 45) && (test[i + 1] == 62)) {
+                System.out.println(String.format("%c%c = IMPLICATION", test[i], test[i+1]));
+                i += 1;
 
             }
-            else if(test[i] == '<' && test[i + 1] == '-' && test[i + 2] == '>'){
-                System.out.println("EQUIVALENCE");
-                i+=2;
 
+            // Tests for whitespace (32 in unicode)
+            else if (test[i] == 32) {
+                System.out.print("");
             }
-            else if (test[i] == '-' && test[i + 1] == '>'){
-                System.out.println("IMPLICATION");
-                i+=1;
-
-            }
-            else if(test[i] == 't' && test[i + 1] == 'e' && test[i + 2] == 's' && test[i + 3] == 't'){
-                System.out.println("BEGIN_TEST");
-                i+=2;
-
-            }
-            else if(Character.isWhitespace(test[i])){
-                continue;
-            }
-            else if(Character.isLetter(test[i])){
-                System.out.println("VARIABLE");
+            // 65 - 90 are capital letters; 97 - 122 are lowercase letters
+            else if (isAlpha(test[i])) {
+                System.out.println(String.format("%c = VARIABLE", test[i]));
             }
 
 
         }
 
+
+
 /*
 Replace above methods with case-insensitivity and unicode range checking.
+List of unicode/dec/chars: https://en.wikipedia.org/wiki/List_of_Unicode_characters
  */
 
     }
