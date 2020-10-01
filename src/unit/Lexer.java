b4c8b4db96f380299635f8e8b66498df71d076e7
Syndikit <maxaustin92@gmail.com>
2020-10-01 18:12:28 -0400

    Modding lexer before I potentially nuke it...

--- src/unit/Lexer.java
+++ src/unit/Lexer.java
@@ -3,15 +3,17 @@ package unit;
 import model.AbstractLexer;
 import java.lang.*;
 
+import static model.AbstractLexer.Tokens.*;
+
 public class Lexer extends model.AbstractLexer {
 	public char[] sent = new char[80];
 
 	//symbols = {'(', ')', ',', '?', '1', '0', '\'', '^', 'v', '='};
 	int[] int_symbols = {40, 41, 44, 63, 49, 48, 39, 94, 118, 61};
-	String[] tokens = {"OPEN_PAREN", "CLOSE_PAREN", "END_BOOL", "END_TEST", "TRUE_LITERAL", "FALSE_LITERAL", "NEGATION", "CONJUNCTION", "DISJUNCTION", "ASSIGNMENT"};
+	Tokens[] tokens = {OPEN_PAREN, CLOSE_PAREN, END_BOOL, END_TEST, TRUE_LITERAL, FALSE_LITERAL, NEGATION, CONJUNCTION, DISJUNCTION, ASSIGNMENT};
 
 	/*
-	Looks through a character array and returns true if the specified character is in said char[].
+	Checks whether character is within the acceptable range of decimal values for letters (upper and lowercase)
 	 */
 	public static boolean isAlpha(char target) {
 
@@ -25,7 +27,9 @@ public class Lexer extends model.AbstractLexer {
 		}
 		return in;
 	}
-
+	/*
+	Specifically looks at whether the lexeme being evaluated is of the bool variety.
+	 */
 	public static boolean isBool(int a, int b, int c, int d) {
 		boolean res = false;
 		if ((a == 98 || a == 66) && (b == 111 || b == 79) && (c == 111 || c == 79) && (d == 108 || d == 76)) {
@@ -34,7 +38,9 @@ public class Lexer extends model.AbstractLexer {
 
 		return res;
 	}
-
+	/*
+        Specifically looks at whether the lexeme being evaluated is of the begin test variety.
+         */
 	public static boolean isTest(int a, int b, int c, int d) {
 		boolean res = false;
 		if ((a == 84 || a == 116) && (b == 69 || b == 101) && (c == 83 || c == 115) && (d == 84 || d == 116)) {
@@ -55,7 +61,7 @@ public class Lexer extends model.AbstractLexer {
 	public void initialize(char[] sentence) {
 
 		for(int i=0; i<sentence.length;i++){
-			sent[i] = Character.toLowerCase(sentence[i]);
+			sent[i] = sentence[i];
 		}
 
 		throw new UnsupportedOperationException();
@@ -71,41 +77,48 @@ public class Lexer extends model.AbstractLexer {
 	 */
 	@Override
 	public void lex() {
+
 		for (int i = 0; i < sent.length; i++) {
 
 			for (int j = 0; j < int_symbols.length; j++) {
 				if (int_symbols[j] == sent[i]) {
-					System.out.println(tokens[j]);
-
+					//System.out.println(tokens[j]);
+					TOKEN = tokens[j];
 				}
 			}
 
 
 			if (((i + 3) < sent.length) && (isBool(sent[i], sent[i + 1], sent[i + 2], sent[i + 3]))) {
-				System.out.println(String.format("%c%c%c%c = BEGIN_BOOL", sent[i], sent[i + 1], sent[i + 2], sent[i + 3]));
+				//System.out.println(String.format("%c%c%c%c = BEGIN_BOOL", sent[i], sent[i + 1], sent[i + 2], sent[i + 3]));
+				TOKEN = BEGIN_BOOL;
 				i += 3;
 
 			} else if (((i + 3) < sent.length) && isTest((int) sent[i], (int) sent[i + 1], (int) sent[i + 2], (int) sent[i + 3])) {
-				System.out.println(String.format("%c%c%c%c = BEGIN_TEST", sent[i], sent[i + 1], sent[i + 2], sent[i + 3]));
+				//System.out.println(String.format("%c%c%c%c = BEGIN_TEST", sent[i], sent[i + 1], sent[i + 2], sent[i + 3]));
+				TOKEN = BEGIN_TEST;
 				i += 3;
 
 			} else if (((i + 2) < sent.length) && (sent[i] == 60) && (sent[i + 1] == 45) && (sent[i + 2] == 62)) {
-				System.out.println(String.format("%c%c%c = EQUIVALENCE", sent[i], sent[i + 1], sent[i+2]));
+				//System.out.println(String.format("%c%c%c = EQUIVALENCE", sent[i], sent[i + 1], sent[i+2]));
+				TOKEN = EQUIVALENCE;
 				i += 2;
 
 			} else if (((i + 1) < sent.length) && (sent[i] == 45) && (sent[i + 1] == 62)) {
-				System.out.println(String.format("%c%c = IMPLICATION", sent[i], sent[i+1]));
+				//System.out.println(String.format("%c%c = IMPLICATION", sent[i], sent[i+1]));
+				TOKEN = IMPLICATION;
 				i += 1;
 
 			}
 
 			// Tests for whitespace (32 in unicode)
 			else if (sent[i] == 32) {
-				System.out.print("");
+				//System.out.print("");
+				continue;
 			}
 			// 65 - 90 are capital letters; 97 - 122 are lowercase letters
 			else if (isAlpha(sent[i])) {
 				System.out.println(String.format("%c = VARIABLE", sent[i]));
+				TOKEN = VARIABLE_NAME;
 			}
 
 
