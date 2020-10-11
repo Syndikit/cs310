6689e346fe3e0dc533a6d01e07cbadf6270692fd
Syndikit <maxaustin92@gmail.com>
2020-10-11 12:50:25 -0400

    Added comments to Parser and initialized Lexer in evaluate()

--- src/unit/Lexer.java
+++ src/unit/Lexer.java
@@ -6,7 +6,8 @@ import java.lang.*;
 import static model.AbstractLexer.Tokens.*;
 
 public class Lexer extends model.AbstractLexer {
-
+	public Tokens[] tokens;
+	private int tok_index;
 	private int index;
 	private char[] S;
 
@@ -38,7 +39,7 @@ public class Lexer extends model.AbstractLexer {
 		S = sentence;
 
 		index = 0;
-
+		tok_index = 0;
 		//throw new UnsupportedOperationException();
 	}
 
@@ -61,77 +62,107 @@ public class Lexer extends model.AbstractLexer {
 		// Tests for ( symbol
 		if (S[index] == 40) {
 			TOKEN = OPEN_PAREN;
+			tokens[tok_index] = TOKEN;
+			tok_index += 1;
 			index+=1;
 		}
 		// Tests for ) symbol
 		else if(S[index] == 41){
 			TOKEN = CLOSE_PAREN;
 			index+=1;
+			tokens[tok_index] = TOKEN;
+			tok_index += 1;
 		}
 		// Tests for , symbol
 		else if(S[index] == 44){
 			TOKEN = END_BOOL;
 			index+=1;
+			tokens[tok_index] = TOKEN;
+			tok_index += 1;
 		}
 		// Tests for ? symbol
 		else if(S[index] == 63){
 			TOKEN = END_TEST;
 			index+=1;
+			tokens[tok_index] = TOKEN;
+			tok_index += 1;
 		}
 		// Tests for 1 character
 		else if(S[index] == 49){
 			TOKEN = TRUE_LITERAL;
 			index+=1;
+			tokens[tok_index] = TOKEN;
+			tok_index += 1;
 		}
 		// Tests for 0 character
 		else if(S[index] == 48){
 			TOKEN = FALSE_LITERAL;
 			index++;
+			tokens[tok_index] = TOKEN;
+			tok_index += 1;
 		}
 		// Tests for ' symbol
 		else if(S[index] == 39){
 			TOKEN = NEGATION;
 			index++;
+			tokens[tok_index] = TOKEN;
+			tok_index += 1;
 		}
 		// Tests for ^ symbol
 		else if(S[index] == 94){
 			TOKEN = CONJUNCTION;
 			index++;
+			tokens[tok_index] = TOKEN;
+			tok_index += 1;
 		}
 		// Tests for v or V character
 		else if((S[index] == 118) || (S[index] == 86)){
 			TOKEN =  DISJUNCTION;
 			index++;
+			tokens[tok_index] = TOKEN;
+			tok_index += 1;
 		}
 		// Tests for = symbol
 		else if((int) this.S[index] == 61){
 			TOKEN = ASSIGNMENT;
 			index++;
+			tokens[tok_index] = TOKEN;
+			tok_index += 1;
 		}
 		// Specifically looks at whether the lexeme being evaluated is of the bool variety and ensures the index for the last char will be inbounds.
 		else if(((index+4) < S.length) && (S[index] == 98 || S[index] == 66) && (S[index+1] == 111 || S[index+1] == 79) && (S[index+2] == 111 || S[index+2] == 79) && (S[index+3] == 108 || S[index+3] == 76)){
 			TOKEN = BEGIN_BOOL;
 			index+=4;
+			tokens[tok_index] = TOKEN;
+			tok_index += 1;
 		}
 		// Specifically looks at whether the lexeme being evaluated is of the test variety and ensures the index for the last char will be inbounds.
 		else if(((index+4) < S.length) && (S[index] == 84 || S[index] == 116) && (S[index+1] == 69 || S[index+1] == 101) && (S[index+2] == 83 || S[index+2] == 115) && (S[index+3] == 84 || S[index+3] == 116)){
 			TOKEN = BEGIN_TEST;
 			index+=4;
+			tokens[tok_index] = TOKEN;
+			tok_index += 1;
 		}
 		// Looks for <-> and ensures the index for the last char will be inbounds.
 		else if(((index+3) < S.length) && (S[index] == 60) && (S[index+1] == 45) && (S[index+2] == 62)){
 			TOKEN = EQUIVALENCE;
 			index+=3;
+			tokens[tok_index] = TOKEN;
+			tok_index += 1;
 		}
 		// Looks for -> and ensures the index for the last char will be inbounds.
 		else if(((index+3) < S.length) && (S[index] == 45) && (S[index+1] == 62)){
 			TOKEN = IMPLICATION;
 			index+=2;
+			tokens[tok_index] = TOKEN;
+			tok_index += 1;
 		}
 		// Looks for alphabetical characters that don't fit the above criteria and are variables
 		else if(isAlpha(S[index])){
 			TOKEN = VARIABLE_NAME;
 			index++;
+			tokens[tok_index] = TOKEN;
+			tok_index += 1;
 		}
 
 		//throw new UnsupportedOperationException("Lex method failed");
--- src/unit/Parser.java
+++ src/unit/Parser.java
@@ -1,20 +1,60 @@
 package unit;
 
+import model.AbstractLexer;
 import model.AbstractLexer.Tokens;
 
 public class Parser extends model.AbstractParser {
+
+	private Lexer Lex;
+	private Tokens[] toks;
+
+	/**
+	 * Instantiates the lexer instance and initializes the lexer's
+	 * input by calling {@link AbstractLexer#initialize(char[])}.
+	 * <p>
+	 * Then evaluates the input as a sentence in the
+	 * language, returning the <code>true</code> or
+	 * <code>false</code> evaluation of the sentence.
+	 * <p>
+	 * This method must throw any exceptions thrown by
+	 * production methods in the recursive descent.
+	 *
+	 * @param sentence A sentence to use as lexer input
+	 * @return The evaluation of the sentence
+	 *
+	 * @throws Exception if the sentence is invalid
+	 */
 	@Override
 	public boolean evaluate(char[] sentence) {
+		Lex.initialize(sentence);
+		Lex.lex();
+		toks = Lex.tokens;
 		// TODO: implement this method stub
 		throw new UnsupportedOperationException();
 	}
 
+	/**
+	 * If the lexer's next token matches the acceptable
+	 * token, returns true and advances the lexer.
+	 * Otherwise, returns false and does not advance.
+	 *
+	 * @param token The acceptable token
+	 * @return Whether the next token is acceptable
+	 */
 	@Override
 	public boolean accept(Tokens token) {
 		// TODO: implement this method stub
 		throw new UnsupportedOperationException();
 	}
 
+	/**
+	 * If the lexer's next token matches the acceptable
+	 * token, advances the lexer. Otherwise, throws an
+	 * <code>IllegalState</code> exception.
+	 *
+	 * @param token The acceptable token
+	 * @return Whether the next token is acceptable
+	 */
 	@Override
 	public void expect(Tokens token) {
 		// TODO: implement this method stub
