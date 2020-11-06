eab53818fef8e90bcef1959ac45fe2c22970a3c7
Syndikit <maxaustin92@gmail.com>
2020-11-06 01:16:55 -0500

    Finished Go version

--- src/Austin-Unit-3.go
+++ src/Austin-Unit-3.go
@@ -16,21 +16,34 @@ func combinations(n int, r int) int{
 	var numerator = factorial(n)
     var rFactorial = factorial(r)
     var nMinusRFactorial = factorial(n-r)
-     return (numerator/(rFactorial*nMinusRFactorial))
+    return (numerator/(rFactorial*nMinusRFactorial))
 }
-/*
-func pancake(n int){
+
+func pancake(n int) int{
 	var n0 int = combinations(n,0)
     var n1 int = combinations(n,1)
 	var n2 int = combinations(n,2)
-	var res1 int = n0 + n1
-	var res2 = 0
-	res2 = res1 + n2
-    return res2
-
+    return n0 + n1 + n2
 }
-   */ 
+
+func cake(n int) int{
+	var pan = pancake(n)
+	var n3 = combinations(n,3)
+	return pan + n3
+}
+
+func hypercake(n int) int{
+	var result = cake(n)
+	var counter = 3
+	
+    for i:= 4; i<n;i++{
+		result += combinations(n,counter)
+	}
+	return result
+}
+
+   
 
 func main(){
-	fmt.Println(pancake(7))
+	fmt.Println(hypercake(7))
 }
